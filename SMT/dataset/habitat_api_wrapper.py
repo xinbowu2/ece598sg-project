7cf08bd71fcdb48a39b5d60548812b1293ad79d5
xinbowu2 <17jasonwu@gmail.com>
2019-11-29 19:31:28 -0600

    update habitat wrapper

--- SMT/dataset/habitat_api_wrapper.py
+++ SMT/dataset/habitat_api_wrapper.py
@@ -3,8 +3,8 @@ import numpy as np
 
 class HabitatWrapper:
 
-  def __init__(self, config):
-    self.env = habitat.Env(config=config)
+  def __init__(self, config, habitat_config):
+    self.env = habitat.Env(config=habitat_config)
 
     self.action_mapping = config['DATASET'].ACTION_MAPPING
     self.action_mapping.update({action_name : index for index, action_name in action_mapping.items()})
@@ -22,11 +22,13 @@ class HabitatWrapper:
 
   def reset(self):
     self.observations = self.env.reset()
-    curr_x, curr_y = self.game_variables['position']
+    self.curr_x, _, self.curr_y = self.env.sim.get_agent_state().position
 
-    self.last_cell_x, self.last_cell_y = curr_x, curr_y
+    self.last_cell_x, self.last_cell_y = self.curr_x, self.curr_y
     self.visited_cells.append([curr_x,curr_y])
     self.is_episode_finished = self.env.episode_over()
+
+    return self.observations
 
   def get_state(self):
     state = self.observations.copy().add('prev_action', self.prev_action)
@@ -44,7 +46,10 @@ class HabitatWrapper:
       if self.current_action != None:
         self.observations = self.env.step(self.action_mapping[self.current_action])
         self.prev_action = self.current_action
+        self.curr_x, _, self.curr_y = self.env.sim.get_agent_state().position
         self.is_episode_finished = self.env.episode_over()
+
+        return self.observations
 
   def is_episode_finished(self):
     return self.is_episode_finished
@@ -53,12 +58,11 @@ class HabitatWrapper:
     return self.prev_action
 
   def get_reward(self):
-    curr_x, curr_y = self.game_variables['position']
     curr_cell_pos = [self.last_cell_x, self.last_cell_y]
 
     # calculate displacement from last cell position
-    dx = curr_x - self.last_cell_x 
-    dy = curr_y - self.last_cell_y
+    dx = self.curr_x - self.last_cell_x 
+    dy = self.curr_y - self.last_cell_y
 
     # calculate new cell position, if the agent enters a new cell
     if np.abs(dx) > self.cell_width:
@@ -78,5 +82,53 @@ class HabitatWrapper:
 
     return self.reward_rate
 
+'''
+if __name__ == '__main__':
+  import random
+
+  from config import config
+  from config import update_config
+
+  def parse_args():
+    parser = argparse.ArgumentParser(description='Train segmentation network')
+    
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        required=True,
+                        type=str)
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+    update_config(config, args)
+
+    return args
+
+  args = parse_args()
+
+  num_steps = 100
+  actions = [random.choice([0,1,2,3 for i in range(num_steps)])]
+
+
+  habitat_config = habitat.get_config(config_file='tasks/pointnav_gibson.yaml')
+  habitat_config.defrost()  
+  habitat_config.DATASET.DATA_PATH = '../data/datasets/pointnav/gibson/v1/val/val.json.gz'
+  habitat_config.DATASET.SCENES_DIR = '../data/scene_datasets/gibson'
+  habitat_config.SIMULATOR.AGENT_0.SENSORS = ['RGB_SENSOR'] 
+  habitat_config.SIMULATOR.TURN_ANGLE = 30
+  habitat_config.ENVIRONMENT.MAX_EPISODE_STEPS = MAX_CONTINUOUS_PLAY*64
+
+  habitat_config.freeze()
+
+  env = HabitatWrapper(config, habitat_config)
+  env.reset()
   
+  for i in range(num_steps):
+    env.set_action(actions[i])
+    env.advance_action()
+'''
+
+    
 
