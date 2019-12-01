5cf5f1a96645eb420d24ecd8b1ce6ac9982be76a
Shah <dpshah2@ccc0080.campuscluster.illinois.edu>
2019-12-01 07:07:52 +0000

    dummy test to test if episode terminates prematurely

--- SMT/dummy_test.py
+++ SMT/dummy_test.py
@@ -6,24 +6,7 @@ import pdb
 import numpy as np
 # set up models 
 tf.keras.backend.set_floatx('float32')
-modalities = ['image']
-modality_dim = {'image': 64}
-reduce_factor = 4
-observation_dim = 128
-depth = 128
-num_heads = 8
-d_model = num_heads*depth
-
-num_actions = 4
-
-scene_memory = models.SceneMemory(modalities=modalities, modality_dim=modality_dim, observation_dim=observation_dim)
-
-policy_network = models.AttentionPolicyNet(num_actions, d_model, num_heads=num_heads)
-
-
-# 256x256x3
-
-
+num_actions = 3
 
 # set up environment
 config = habitat.get_config(config_file='tasks/pointnav_gibson.yaml')
@@ -33,12 +16,13 @@ config.DATASET.SCENES_DIR = '../data/scene_datasets/gibson'
 config.SIMULATOR.AGENT_0.SENSORS = ['RGB_SENSOR'] 
 config.SIMULATOR.TURN_ANGLE = 30
 config.ENVIRONMENT.MAX_EPISODE_STEPS = 100
+#config.SIMULATOR.SCENE = "data/scene_datasets/gibson/Lynchburg.glb"
 
 config.freeze()
 env = habitat.Env(config=config)
 r = random.randint(1, len(env.episodes))
 env._current_episode = env.episodes[r]
-
+print("ENVIRONMENT EPISODES", len(env.episodes))
 action_mapping = {      
       0: 'move_forward',
       1: 'turn left',
@@ -46,23 +30,12 @@ action_mapping = {
       3: 'stop'
 }
 
-from tensorflow.keras.optimizers import Adam
-optimizer = Adam()
-from tensorflow.keras.losses import MSE 
-loss_function = MSE
-from models import RL_Agent
-
-agent = RL_Agent(env, optimizer, loss_function)
-agent.reset(109)
-
-agent.step(0)
-
-observations = {}
-current_x = env.reset()['rgb']/255.0
-observations['image'] = current_x
-
-current_embedding, mem = scene_memory(observations)
-new_x = env.step(1)['rgb']/255.0
-observations['image'] = new_x
-current_embedding, mem = scene_memory(observations)
-
+i=0
+current_x = env.reset()['rgb']/255.0 
+while True:    
+    action_index = random.randint(0, 2)
+    current_x = env.step(action_index)['rgb']/255.0
+    if env.episode_over:
+        break
+    print(i)
+    i+=1
