69032aee10141795853b037fb457b45468eb1161
Hurwit <hurwit2@ccc0080.campuscluster.illinois.edu>
2019-12-04 19:48:48 +0000

    testing resnet

--- SPTM-Tensorflow2/src/train/train_resnet_cifar10.py
+++ SPTM-Tensorflow2/src/train/train_resnet_cifar10.py
@@ -3,6 +3,7 @@ from train.train_setup import *
 
 def preprocess(x, y):
   x = tf.image.per_image_standardization(x)
+  x = tf.cast(x, tf.float32)
   return x, y
 
 
@@ -13,8 +14,14 @@ def main():
     train_dataset = tf.data.Dataset.from_tensor_slices((x, y)).map(preprocess).shuffle(50000).batch(128, drop_remainder=True)
     test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).map(preprocess).batch(128, drop_remainder=True)
 
-    model = ResNet18(3)
-    adam = tf.keras.optimizers.Adam(lr=LEARNING_RATE, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    base_model = tf.keras.applications.resnet50.ResNet50(weights= None, include_top=False, input_shape= (32,32,3))
+    x = base_model.output
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Dropout(0.3)(x)
+    predictions = tf.keras.layers.Dense(10, activation= 'softmax')(x)
+    model = tf.keras.models.Model(inputs = base_model.input, outputs = predictions)
+    #model = ResNet18(10)
+    adam = tf.keras.optimizers.Adam(lr=LEARNING_RATE)
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
     model.fit(train_dataset,
