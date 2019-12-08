5bad7030a58b0f5b54c0b252c9e61761f020942d
Hurwit <hurwit2@cc-login1.campuscluster.illinois.edu>
2019-12-08 22:36:54 +0000

    fixed issues with test_sptm

--- SPTM-Tensorflow2/src/test_sptm.py
+++ SPTM-Tensorflow2/src/test_sptm.py
@@ -15,6 +15,9 @@ action_mapping = {
 	3: '"Stop"'
 }
 
+MAX_EDGE_DIST = 1
+NEG_SAMPLE_MULT = 5
+
 def distance(pos1, pos2):
 	return math.sqrt((pos2[0] - pos1[0])**2 + (pos2[1] - pos1[1])**2)
 
@@ -59,7 +62,8 @@ def visualize_action_prediction(index1, index2, images, actions):
 
 	current_x = images[index1]
 	future_x = images[index2]
-	prediction = action_predictor.predict(np.concatenate((previous_x, current_x, future_x), axis=2)).argmax(axis=-1)
+	sample = np.concatenate((previous_x, current_x, future_x), axis=2)
+	prediction = action_predictor.predict(np.array([sample])).argmax(axis=-1)[0]
 	action = actions[index1]
 	fig = plt.figure(figsize=(75,75))
 	sub = fig.add_subplot(1,2,1)
@@ -71,7 +75,6 @@ def visualize_action_prediction(index1, index2, images, actions):
 	fig.savefig('action_prediction.png')
 
 def test_edge_predictor(images, actions, positions):
-
 	batch_x = []
 	batch_y = []
 	for index1 in range(0, len(images) - 1):
@@ -79,19 +82,27 @@ def test_edge_predictor(images, actions, positions):
 			current_x = images[index1]
 			future_x = images[index2]
 
-			batch_x.append(np.concatenate((current_x, future_x), axis=2))
-			batch_y.append(index2 - index < 8)
-			if len(batch) == 64 or (index1 == len(images) - 2 and index2 == len(images) - 2):
-				print(edge_predictor.test_on_batch(x, y, sample_weight=None, reset_metrics=False))
+			dist = distance(positions[index1], positions[index2])
+			if dist < MAX_EDGE_DIST:
+				batch_x.append(np.concatenate((current_x, future_x), axis=2))
+				batch_y.append(1)
+			elif dist > MAX_EDGE_DIST * NEG_SAMPLE_MULT:
+				batch_x.append(np.concatenate((current_x, future_x), axis=2))
+				batch_y.append(0)
+			if len(batch_x) == 64 or (index1 == len(images) - 2 and index2 == len(images) - 2):
+				batch_x = np.array(batch_x)
+				batch_y = np.array(batch_y)	
+				print(edge_predictor.evaluate(batch_x, batch_y, batch_size=len(batch_x)))
 				batch_x = []
 				batch_y = []
 
-def visualize_edge_predictor(index1, index2, images, positions):
+def visualize_edge_prediction(index1, index2, images, positions):
 	fig = plt.figure()
 	plt.plot(*positions.T, 'b-')
 	plt.plot([positions[index1,0], positions[index2,0]], [positions[index1,1], positions[index2,1]],'r-')
 	plt.annotate("Start", positions[index1], textcoords="offset points", xytext=(0,10), ha='center')
 	plt.annotate("End", positions[index2], textcoords="offset points", xytext=(0,10), ha='center')
+	plt.show()
 	fig.savefig('edge_prediction_trajectory.png')
 
 	fig = plt.figure(figsize=(75,75))
@@ -102,6 +113,7 @@ def visualize_edge_predictor(index1, index2, images, positions):
 	sub.set_title('End')
 	sub.imshow(images[index2], interpolation='nearest')
 	# fig.suptitle('Distance: ' + str(distance(positions[index1], positions[index2])), fontsize=12)
+	plt.show()
 	fig.savefig('edge_prediction_images.png')
 
 if __name__ == '__main__':
@@ -113,7 +125,6 @@ if __name__ == '__main__':
 	for im_path in glob.glob(trajectory_dir + "/images/*.png"):
 		image_paths.append(im_path)
 	image_paths.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-	print(image_paths)
 	images = [center_crop_resize(plt.imread(x)[:,:,:3], 256) for x in image_paths][:-1]
 	actions = np.load(trajectory_dir + '/actions.npy', allow_pickle=True)[:-1]
 	positions = np.load(trajectory_dir + '/positions.npy', allow_pickle=True)[:-1]
@@ -121,6 +132,6 @@ if __name__ == '__main__':
 	assert len(images) == len(actions)+1 == len(positions), 'Length of inputs not the same'
 
 	# test_action_predictor(images, actions)
-	visualize_action_prediction(50, 51, images, actions)
-
-
+	# visualize_action_prediction(0, 1, images, actions)
+	test_edge_predictor(images, actions, positions)
+	#visualize_edge_prediction(100, 110, images, positions)
