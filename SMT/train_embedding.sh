4f59761fdeae2135332a1873eb21712cf1d2869b
xinbowu2 <17jasonwu@gmail.com>
2019-12-09 02:26:29 -0600

    add scripts for training and debugging

---
+++ SMT/train_embedding.sh
@@ -0,0 +1,4 @@
+source activate habitat
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/xinbowu2/scratch/ece598sg-project/cuda/lib64
+cd ece598sg-project/SMT
+python training_embedding_with_env_wrapper_3.py --cfg experiments/smt_gibson_debug.yaml 
---
+++ SMT/train_policy.sh
@@ -0,0 +1,3 @@
+source activate habitat
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/xinbowu2/scratch/ece598sg-project/cuda/lib64
+python training_policy_with_env_wrapper_al.py --cfg experiments/smt_gibson_debug.yaml 
